03b257a1ae84cd252e38c288fd734e43baf14ece
giangnguyen2110 <giangnguyen21102005@gmail.com>
2026-08-22 09:57:35 +0700

    Thêm file để deploy vercel

--- modern/src/app/app-routing.module.ts
+++ modern/src/app/app-routing.module.ts
@@ -6,13 +6,18 @@ import { LayoutComponent } from './layouts/layout.component';
 // Auth
 import { AuthGuard } from './core/guards/auth.guard';
 
-const routes: Routes = [
+/*const routes: Routes = [
   { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [AuthGuard] },
   { path: 'auth', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)  },
   { path: 'pages', loadChildren: () => import('./extraspages/extraspages.module').then(m => m.ExtraspagesModule), canActivate: [AuthGuard] },
   { path: 'landing', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)},
+];?*/
+const routes: Routes = [
+  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  { path: 'auth', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)  },
+  { path: 'pages', loadChildren: () => import('./extraspages/extraspages.module').then(m => m.ExtraspagesModule) },
+  { path: 'landing', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)},
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
