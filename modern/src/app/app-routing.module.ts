ca8f40de1af42765b07d070ff67af930c953ce4a
giangnguyen2110 <giangnguyen21102005@gmail.com>
2026-08-22 10:05:42 +0700

    Thêm file để deploy vercel

--- modern/src/app/app-routing.module.ts
+++ modern/src/app/app-routing.module.ts
@@ -4,20 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layouts/layout.component';
 
 // Auth
-import { AuthGuard } from './core/guards/auth.guard';
+/*import { AuthGuard } from './core/guards/auth.guard';*/
 
-/*const routes: Routes = [
-  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [AuthGuard] },
-  { path: 'auth', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)  },
-  { path: 'pages', loadChildren: () => import('./extraspages/extraspages.module').then(m => m.ExtraspagesModule), canActivate: [AuthGuard] },
-  { path: 'landing', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)},
-];?*/
 const routes: Routes = [
-  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)/*, canActivate: [AuthGuard]*/},
   { path: 'auth', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)  },
-  { path: 'pages', loadChildren: () => import('./extraspages/extraspages.module').then(m => m.ExtraspagesModule) },
+  { path: 'pages', loadChildren: () => import('./extraspages/extraspages.module').then(m => m.ExtraspagesModule), /*canActivate: [AuthGuard]*/ },
   { path: 'landing', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)},
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
